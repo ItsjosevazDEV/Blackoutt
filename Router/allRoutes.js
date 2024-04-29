8dab2b3c75d596bd2ec2c7705b57951d8f3efcc7
Jose <rosesmanagementpro@gmail.com>
2024-04-29 21:15:52 +0200

    Logo and events add

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/events", " Les Events", "/pages/events.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Blackoutt Lounge";
