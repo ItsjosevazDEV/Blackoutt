c1ef6ae95285639ffda36b6ca7be2adf23d62008
Jose <rosesmanagementpro@gmail.com>
2024-05-24 13:04:32 +0200

    Ajout authorize Routes

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,10 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize;
     }
 }
+
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,15 +2,15 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/menu", "Menu", "/pages/menu.html"),
-    new Route("/events", "Events", "/pages/events.html"),
-    new Route("/allResa", "Réservations", "/pages/reservations/allResa.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html" , "/js/auth/signup.js"), 
-    new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "Changement mot de passe", "/pages/auth/editPassword.html"),
-    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/menu", "Menu", "/pages/menu.html", []),
+    new Route("/events", "Events", "/pages/events.html", []),
+    new Route("/allResa", "Réservations", "/pages/reservations/allResa.html", ["client", "admin"]),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html",["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html",["disconnected"], "/js/auth/signup.js"), 
+    new Route("/account", "Mon compte", "/pages/auth/account.html",["client"]),
+    new Route("/editPassword", "Changement mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html", ["client"]),
 
 ];
 
